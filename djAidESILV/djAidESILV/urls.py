0730b19c8db1a4d50c0139b87a6d7b6b8290a811
Kulumbaf <francis.kulumba@edu.devinci.fr>
2020-05-09 08:22:56 +0200

    Structure de django, page d'inscription

--- djAidESILV/djAidESILV/urls.py
+++ djAidESILV/djAidESILV/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from djAidESILV.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accueil.urls')), #Le nom de domaine de base renvoie vers le fichier home.urls
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
 ]
