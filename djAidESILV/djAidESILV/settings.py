f0254e6c366d3f20510a4629d84ac836f79e0bfc
Kulumbaf <francis.kulumba@edu.devinci.fr>
2020-05-15 02:39:13 +0200

    Changements mineurs

--- djAidESILV/djAidESILV/settings.py
+++ djAidESILV/djAidESILV/settings.py
@@ -50,7 +50,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'djAidESILV/templates'),
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
